35497654c143c9a32bec2ef926aeb801010439c7
Lena <lenaskarlat@gmail.com>
2014-12-09 23:15:42 +0200

    Started working on MyTree

--- 04_TDD/src/main/impl/MyTree.java
+++ 04_TDD/src/main/impl/MyTree.java
@@ -4,27 +4,32 @@ import main.interfaces.ITree;
 
 public class MyTree<T> implements ITree<T> {
 
+	
+	
 	@Override
 	public void add(T element) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("I'm wokring on this.");
 	}
 
 	@Override
 	public T remove(T element) {
 		// TODO Auto-generated method stub
+		System.out.println("I'm wokring on this.");
 		return null;
 	}
 
 	@Override
 	public boolean contains(T element) {
 		// TODO Auto-generated method stub
+		System.out.println("I'm wokring on this.");
 		return false;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
+		System.out.println("I'm wokring on this.");
 		return 0;
 	}
 
